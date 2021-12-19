4b5d5cf674e45c57f3ffb14d7d9bce3cbe0aa8cf
jonathan <jonathan.gabin.auguste@hotmail.fr>
2021-12-19 20:52:49 +0100

    Import

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\ExpenseRepository;
-use Omines\DataTablesBundle\DataTableFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
