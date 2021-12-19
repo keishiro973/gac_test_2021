5ad81d39d4491a74766eaf4eb173a76da8942958
jonathan <jonathan.gabin.auguste@hotmail.fr>
2021-12-19 21:46:36 +0100

    dashboard Form

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\DashboardSearchType;
 use App\Repository\ExpenseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,21 +14,36 @@ class DashboardController extends AbstractController
     /**
      * @Route("/dashboard", name="dashboard")
      */
-    public function index(ExpenseRepository $expenseRepository): Response
+    public function index(Request $request, ExpenseRepository $expenseRepository): Response
     {
-        $now = new \DateTime();
-        $lastMonth = new \DateTime('last month');
-        $totalTe = $expenseRepository->totalTeBetween($lastMonth, $now);
-        $totalTi = $expenseRepository->totalTiBetween($lastMonth, $now);
-        $byCategory = $expenseRepository->sumByCategory($lastMonth, $now);
-        $top10Vehicle = $expenseRepository->top10Vehicle($lastMonth, $now);
+        $startDate = new \DateTime('last month');
+        $endDate = new \DateTime();
+
+        $form = $this->createForm(DashboardSearchType::class, [
+            'BeginDate' => $startDate,
+            'endDate' => $endDate,
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formDatas = $form->getData();
+            $startDate = $formDatas['BeginDate'];
+            $endDate = $formDatas['endDate'];
+        }
+
+        $totalTe = $expenseRepository->totalTeBetween($startDate, $endDate);
+        $totalTi = $expenseRepository->totalTiBetween($startDate, $endDate);
+        $byCategory = $expenseRepository->sumByCategory($startDate, $endDate);
+        $top10Vehicle = $expenseRepository->top10Vehicle($startDate, $endDate);
 
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
             'totalTe' => $totalTe[0],
-            'totalTi' => $totalTi[0]                        ,
+            'totalTi' => $totalTi[0],
             'byCategory' => $byCategory,
             'top10Vehicle' => $top10Vehicle,
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/DashboardSearchType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DashboardSearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('BeginDate', DateTimeType::class, [
+                    'label' => 'Date de début',
+                ]
+            )
+            ->add('endDate', DateTimeType::class, [
+                'label' => 'Date de fin'
+            ])
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+
+}
